310248e2ef680a54ff1a3fb413d9b95842ef23a3
amazingandyyy <amazingandyyy@gmail.com>
2016-04-24 21:53:07 -0700

    initialize

---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const PORT = process.env.PORT || 8000;
+
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'));
+app.set('view engine', 'jade');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: "Welcome",
+        theme: "readable"
+    });
+});
+
+
+app.listen(PORT, err => {
+    console.log(err || `Server listening on portttt ${PORT}`);
+});
