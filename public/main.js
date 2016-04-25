6d235a75c2a4f9e701e6acbc4b1fae3d5efad284
amazingandyyy <amazingandyyy@gmail.com>
2016-04-24 23:31:44 -0700

    move fb init()
    move fb init()

--- public/main.js
+++ public/main.js
@@ -4,40 +4,40 @@ $(document).ready(init);
 
 function init() {
     startingAnimation();
-
 }
-
-function guid() {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-    }
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-        s4() + '-' + s4() + s4() + s4();
-}
-
-FB.init({
-    appId: '{200466797006747}',
-    status: true,
-    xfbml: true,
-    version: 'v2.4' // or v2.0, v2.1, v2.2, v2.3
-});
-
 
 
 function myFacebookLogin() {
-        FB.api('/840783929399087/feed',
-            'post', {
-                message: 'Hello, world!'
-            },
-            function(response) {
-                if (response && !response.error) {
-                    console.log('response.error: ', response.error);
-                }
+    FB.api('/840783929399087/feed',
+        'post', {
+            message: 'Hello, world!'
+        },
+        function(response) {
+            if (response && !response.error) {
+                console.log('response.error: ', response.error);
             }
-        );
-    };
+        }
+    );
+};
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+
+
+
+
+
+
 
 function startingAnimation() {
     var intro = $('.intro')
